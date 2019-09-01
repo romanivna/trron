a81435d47532f425083118ffe45cc106aa818365
OlecsandrStelmakh <saniast05@gmail.com>
2019-09-01 19:41:48 +0300

    fix attachment navigation at the top of the screen on your phone

--- js/header.js
+++ js/header.js
@@ -1,19 +1,40 @@
 window.addEventListener('scroll', function() {
   let scroll = pageYOffset;
   let width = screen.width;
-  if ( scroll > 150){
-      document.getElementsByClassName("header-wrapper--grey")[0].classList.add("header-wrapper--grey--fixed");
-      document.getElementsByClassName("header-nav-catalog")[0].classList.add("header-nav-catalog--unset");
-      document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.add("header-nav-catalog-list-list--scroll");
-      if (width > 884){
-        document.getElementsByClassName("header-nav-scroll")[0].classList.add("header-nav-scroll--show");
-        document.getElementsByClassName("header-nav-menu-name")[0].classList.add("header-nav-menu-name--none");   
-      }
-  } else {
-      document.getElementsByClassName("header-wrapper--grey")[0].classList.remove("header-wrapper--grey--fixed");
-      document.getElementsByClassName("header-nav-menu-name")[0].classList.remove("header-nav-menu-name--none");
-      document.getElementsByClassName("header-nav-scroll")[0].classList.remove("header-nav-scroll--show");
-      document.getElementsByClassName("header-nav-catalog")[0].classList.remove("header-nav-catalog--unset");
-      document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.remove("header-nav-catalog-list-list--scroll");
-  }
+//   if (scroll > 150){
+//       document.getElementsByClassName("header-wrapper--grey")[0].classList.add("header-wrapper--grey--fixed");
+//       document.getElementsByClassName("header-nav-catalog")[0].classList.add("header-nav-catalog--unset");
+//       document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.add("header-nav-catalog-list-list--scroll");
+//       if (width > 870){
+//         document.getElementsByClassName("header-nav-scroll")[0].classList.add("header-nav-scroll--show");
+//         document.getElementsByClassName("header-nav-menu-name")[0].classList.add("header-nav-menu-name--none");   
+//       }
+//   } else {
+//       document.getElementsByClassName("header-wrapper--grey")[0].classList.remove("header-wrapper--grey--fixed");
+//       document.getElementsByClassName("header-nav-menu-name")[0].classList.remove("header-nav-menu-name--none");
+//       document.getElementsByClassName("header-nav-scroll")[0].classList.remove("header-nav-scroll--show");
+//       document.getElementsByClassName("header-nav-catalog")[0].classList.remove("header-nav-catalog--unset");
+//       document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.remove("header-nav-catalog-list-list--scroll");
+//   }
+// });
+
+
+
+if (scroll > 150 && width > 880) {
+    document.getElementsByClassName("header-wrapper--grey")[0].classList.add("header-wrapper--grey--fixed");
+    document.getElementsByClassName("header-nav-catalog")[0].classList.add("header-nav-catalog--unset");
+    document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.add("header-nav-catalog-list-list--scroll");
+    document.getElementsByClassName("header-nav-scroll")[0].classList.add("header-nav-scroll--show");
+    document.getElementsByClassName("header-nav-menu-name")[0].classList.add("header-nav-menu-name--none");
+} else if (scroll > 247 && width < 880){
+        document.getElementsByClassName("header-wrapper--grey")[0].classList.add("header-wrapper--grey--fixed");
+        document.getElementsByClassName("header-nav-catalog")[0].classList.add("header-nav-catalog--unset");
+        document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.add("header-nav-catalog-list-list--scroll");
+} else {
+        document.getElementsByClassName("header-wrapper--grey")[0].classList.remove("header-wrapper--grey--fixed");
+        document.getElementsByClassName("header-nav-menu-name")[0].classList.remove("header-nav-menu-name--none");
+        document.getElementsByClassName("header-nav-scroll")[0].classList.remove("header-nav-scroll--show");
+        document.getElementsByClassName("header-nav-catalog")[0].classList.remove("header-nav-catalog--unset");
+        document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.remove("header-nav-catalog-list-list--scroll");
+}
 });
