2b6ea7123955e60a917c9339be13ca24a6d6e4c5
OlecsandrStelmakh <saniast05@gmail.com>
2019-09-01 19:04:05 +0300

    fix

--- js/header.js
+++ js/header.js
@@ -1,15 +1,16 @@
 window.addEventListener('scroll', function() {
   let scroll = pageYOffset;
   let width = screen.width;
-  if ( scroll > 150 && width > 884 ){
-      document.getElementsByClassName("header-wraper--grey")[0].classList.add("header-wraper--grey--fixed");
-      document.getElementsByClassName("header-nav-menu-name")[0].classList.add("header-nav-menu-name--none");
-      document.getElementsByClassName("header-nav-scroll")[0].classList.add("header-nav-scroll--show");
+  if ( scroll > 150){
+      document.getElementsByClassName("header-wrapper--grey")[0].classList.add("header-wrapper--grey--fixed");
       document.getElementsByClassName("header-nav-catalog")[0].classList.add("header-nav-catalog--unset");
       document.getElementsByClassName("header-nav-catalog-list-list")[0].classList.add("header-nav-catalog-list-list--scroll");
-  }
-  if ( scroll < 149 ){
-      document.getElementsByClassName("header-wraper--grey")[0].classList.remove("header-wraper--grey--fixed");
+      if (width > 884){
+        document.getElementsByClassName("header-nav-scroll")[0].classList.add("header-nav-scroll--show");
+        document.getElementsByClassName("header-nav-menu-name")[0].classList.add("header-nav-menu-name--none");   
+      }
+  } else {
+      document.getElementsByClassName("header-wrapper--grey")[0].classList.remove("header-wrapper--grey--fixed");
       document.getElementsByClassName("header-nav-menu-name")[0].classList.remove("header-nav-menu-name--none");
       document.getElementsByClassName("header-nav-scroll")[0].classList.remove("header-nav-scroll--show");
       document.getElementsByClassName("header-nav-catalog")[0].classList.remove("header-nav-catalog--unset");
