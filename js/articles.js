66357051927c0aec7bab1eb64432f8a0f3569a40
OlecsandrStelmakh <saniast05@gmail.com>
2019-09-04 09:35:01 +0300

    Fix problem with feature/articles

--- js/articles.js
+++ js/articles.js
@@ -4,68 +4,60 @@ fetch('jsons/articles.json')
   })
   .then(data => {
     // Work with JSON data here
-    articlesCounter = data["articles"].length;
-    let a = Math.floor(Math.random() * articlesCounter);
-    let b = Math.floor(Math.random() * articlesCounter);
-    let c = Math.floor(Math.random() * articlesCounter);
+    const articlesCounter = data["articles"].length;
+    const firstArticle = Math.floor(Math.random() * articlesCounter);
+    const secondArticle = Math.floor(Math.random() * articlesCounter);
+    const thirdArticle= Math.floor(Math.random() * articlesCounter);
 
     const width = screen.width;
     const articles = document.getElementsByClassName("articles")[0];
-    if (width>=800){
-      bildArticle(a);
-      bildArticle(b);
-      bildArticle(c);
-    } else if(width<400){
-      bildArticle(a);
-    } else{
-      bildArticle(a);
-      bildArticle(b);
-    }
-
-    function bildArticle(i){
-      let articlesConteiner = document.createElement("div");
-      articlesConteiner.classList.add("articles-conteiner");
+      bildArticle(firstArticle);
+      bildArticle(secondArticle);
+      bildArticle(thirdArticle);
+    function bildArticle(articleIndex ){
+      let articlesContainer = document.createElement("div");
+      articlesContainer.classList.add("articles-container");
 
       let imgLink = document.createElement("a");
-      imgLink.href = "#article$"+data["articles"][i]["id"];
+      imgLink.href = "articles.html?"+data["articles"][articleIndex ].id;
 
-      let articlesImgConteiner = document.createElement("div");
-      articlesImgConteiner.classList.add("articles-img-conteiner");
+      let articlesImgContainer = document.createElement("div");
+      articlesImgContainer.classList.add("articles-img-container");
 
       let img = document.createElement("img");
       img.classList.add("articles-img");
-      img.src = data["articles"][i]["image"];
-      articlesImgConteiner.appendChild(img);
-      imgLink.appendChild(articlesImgConteiner);
-      articlesConteiner.appendChild(imgLink);
+      img.src = data["articles"][articleIndex].image ;
+      img.title = data["articles"][articleIndex].title;
+      articlesImgContainer.appendChild(img);
+      imgLink.appendChild(articlesImgContainer);
+      articlesContainer.appendChild(imgLink);
     
       let articlesDate = document.createElement("span");
       articlesDate.classList.add("articles-date");
-      articlesDate.innerHTML = data["articles"][i]["date"];
-      articlesConteiner.appendChild(articlesDate);
+      articlesDate.innerHTML = data["articles"][articleIndex].date;
+      articlesContainer.appendChild(articlesDate);
 
       let articlesDescription = document.createElement("div");
       articlesDescription.classList.add("articles-description");
 
       let articlesDescriptionTitle = document.createElement("span");
       articlesDescriptionTitle.classList.add("articles-description-title");
-      articlesDescriptionTitle.innerHTML = data["articles"][i]["title"];
+      articlesDescriptionTitle.innerHTML = data["articles"][articleIndex].title;
       articlesDescription.appendChild(articlesDescriptionTitle);
 
       let articlesDescriptionText = document.createElement("p");
       articlesDescriptionText.classList.add("articles-description-text");
-      articlesDescriptionText.innerHTML = data["articles"][i]["text"];
+      articlesDescriptionText.innerHTML = data["articles"][articleIndex].text;
       articlesDescription.appendChild(articlesDescriptionText);
 
       let articlesDescriptionId = document.createElement("a");
       articlesDescriptionId.classList.add("articles-description-id");
       articlesDescriptionId.innerHTML = "more details";
-      articlesDescriptionId.href = "#article$"+data["articles"][i]["id"];
+      articlesDescriptionId.href = "articles.html?"+data["articles"][articleIndex].id;
       articlesDescription.appendChild(articlesDescriptionId);
-      articlesConteiner.appendChild(articlesDescription);
-      articles.appendChild(articlesConteiner);
+      articlesContainer.appendChild(articlesDescription);
+      articles.appendChild(articlesContainer);
     };
   })
   .catch(err => {
-    alert ("Something went wrong please try again");
   })
