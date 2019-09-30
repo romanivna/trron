51499d0f6558428dc1673a20d009416d9ec038f9
Roman Saldan <saldanroman@gmail.com>
2019-09-30 18:57:07 +0300

    updated and  now  supported IE

--- js/article.js
+++ js/article.js
@@ -6,19 +6,11 @@ let articleArrObj;
     if (xhr.readyState === 4 && xhr.status === 200) {
       articleArrObj = JSON.parse(xhr.response);
       makeArticle();
-      console.log(articleArrObj);
     }
   };
-  xhr.open("GET", "./jsons/articles.json", true);
+  xhr.open("GET", "../jsons/articles.json", true);
   xhr.send();
 })();
-
-// fetch("./jsons/articles.json")
-//   .then(response => response.json())
-//   .then(data => {
-//     articleArrObj = data;
-//     makeArticle();
-//   });
 
 function makeArticle() {
   let searchableIDOfObject = +window.location.search.slice(1);
@@ -37,16 +29,21 @@ function makeArticle() {
   image.setAttribute("src", articleObj[0].image);
   image.setAttribute("alt", articleObj[0].title);
   image.setAttribute("class", "article-content-image");
-
   const title = document.createElement("p");
   title.setAttribute("class", "article-content-title");
   title.innerText = articleObj[0].title;
-
   const text = document.createElement("p");
   text.setAttribute("class", "article-content-text");
   text.innerText = articleObj[0].text;
-
   articleContent.appendChild(image);
   articleContent.appendChild(title);
   articleContent.appendChild(text);
+  document.querySelector(".nav-links-add").innerHTML =
+    "» " +
+    '<a href="blog.html#' +
+    articleObj[0].categorie +
+    '" class="nav-links">' +
+    " " +
+    articleObj[0].categorie +
+    "</a>";
 }
