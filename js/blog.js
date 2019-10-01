b4d2f7a5dd11fa4a158166c864afb8c89bc3a0f9
Roman Saldan <saldanroman@gmail.com>
2019-10-01 11:55:27 +0300

    remake blog and supported IE

--- js/blog.js
+++ js/blog.js
@@ -1,96 +1,90 @@
 "use strict";
-let arrayOfAllArticles;
-let arrayOfArticles;
-let sumOfPages;
 (function ajaxGetJson() {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      arrayOfArticles = JSON.parse(xhr.response).articles;
-      arrayOfAllArticles = JSON.parse(xhr.response).articles;
-      const serchLine = window.location.hash.slice(1).replace(/%20/g, " ");
-      createPages();
-      switchPages();
-      createContent(arrayOfArticles, 0);
-      document.querySelectorAll(".blog-links").forEach(function(item) {
-        if (item.innerText === serchLine) {
-          item.click();
-        }
-      });
+      window.arrayOfAllArticles = JSON.parse(xhr.response).articles;
+      window.serchLine = window.location.search.slice(1).replace(/%20/g, " ");
+      window.indexOfStartArticles = 1;
+      if (serchLine.indexOf("=") !== -1) {
+        const serchLineIndex = serchLine.slice(serchLine.indexOf("="));
+        serchLine = serchLine.replace(serchLineIndex, "");
+        indexOfStartArticles = +serchLineIndex.slice(1);
+      }
+      if (serchLine) {
+        arrayOfAllArticles = JSON.parse(xhr.response).articles.filter(function(
+          item
+        ) {
+          return item.categorie === serchLine;
+        });
+      }
+      createLinksOfPages();
+      createContent(arrayOfAllArticles, indexOfStartArticles);
     }
   };
   xhr.open("GET", "../jsons/articles.json", true);
   xhr.send();
 })();
 
-function createPages() {
-  let articlescount = 0;
-  window.pagesOfContent = document.querySelector(".blog-pages");
-  sumOfPages = Math.ceil(arrayOfArticles.length / 8);
-  if (arrayOfArticles.length <= 8) {
+function createLinksOfPages() {
+  const pagesOfContent = document.querySelector(".blog-pages");
+  let sumOfPages = Math.ceil(arrayOfAllArticles.length / 8);
+  if (indexOfStartArticles > 1) {
+    const prevPage = document.createElement("a");
+    prevPage.setAttribute(
+      "href",
+      "blog.html?" + serchLine + "=" + (indexOfStartArticles - 1)
+    );
+    prevPage.setAttribute(
+      "class",
+      "blog-pages-links blog-pages-links--prevButton"
+    );
+    prevPage.innerText = "‹ prev page";
+    pagesOfContent.appendChild(prevPage);
+  }
+  if (arrayOfAllArticles.length <= 8) {
     return;
   }
   for (let indexOfPage = 1; indexOfPage <= sumOfPages; indexOfPage++) {
     const pageIndex = document.createElement("a");
-    pageIndex.setAttribute("href", "#");
-    pageIndex.setAttribute("class", "blog-pages-links");
-    pageIndex.setAttribute("data", articlescount);
+    pageIndex.setAttribute(
+      "href",
+      "blog.html?" + serchLine + "=" + indexOfPage
+    );
+    pageIndex.setAttribute(
+      "class",
+      "blog-pages-links blog-pages-links--numbers"
+    );
     pageIndex.innerText = indexOfPage;
     pagesOfContent.appendChild(pageIndex);
-    articlescount += 8;
   }
-  document
-    .querySelectorAll(".blog-pages-links")[0]
-    .classList.add("blog-pages-links--active");
-  const previousPage = document.createElement("p");
-  previousPage.setAttribute("class", "blog-pages-links");
-  previousPage.setAttribute("onclick", "changePages(this)");
-  previousPage.setAttribute("data-next-page", 1);
-  previousPage.setAttribute("direction", "prev");
-  previousPage.style.cursor = "pointer";
-  previousPage.style.width = "120px";
-  previousPage.style.display = "none";
-  previousPage.innerText = "‹ previous page";
-  pagesOfContent.prepend(previousPage);
-  const nextPage = document.createElement("p");
-  nextPage.setAttribute("class", "blog-pages-links");
-  nextPage.setAttribute("onclick", "changePages(this)");
-  nextPage.setAttribute("data-next-page", 1);
-  nextPage.setAttribute("direction", "next");
-  nextPage.style.cursor = "pointer";
-  nextPage.style.width = "90px";
-  nextPage.innerText = " next page ›";
+  const nextPage = document.createElement("a");
+  nextPage.setAttribute(
+    "href",
+    "blog.html?" + serchLine + "=" + (indexOfStartArticles + 1)
+  );
+  nextPage.setAttribute(
+    "class",
+    "blog-pages-links blog-pages-links--nextButton"
+  );
+  nextPage.innerText = "next page ›";
   pagesOfContent.appendChild(nextPage);
+  const activePages = document.querySelectorAll(".blog-pages-links--numbers");
+  if (activePages.length === indexOfStartArticles) {
+    nextPage.parentNode.removeChild(nextPage);
+  }
+  activePages[indexOfStartArticles - 1].classList.add(
+    "blog-pages-links--active"
+  );
 }
 
-function switchPages() {
-  const pageLinks = document.querySelectorAll("a.blog-pages-links");
-  pageLinks.forEach(function(item) {
-    item.addEventListener("click", function(event) {
-      pageLinks.forEach(function(item) {
-        item.classList.remove("blog-pages-links--active");
-      });
-      event.target.classList.add("blog-pages-links--active");
-      document.querySelector(".blog-content").innerHTML = "";
-      createContent(arrayOfArticles, +item.getAttribute("data"));
-      const buttonPrevNext = document.querySelectorAll("p.blog-pages-links");
-      buttonPrevNext[0].setAttribute("data-next-page", item.innerText);
-      if (+item.innerText > 1) {
-        buttonPrevNext[0].style.display = "flex";
-      } else {
-        buttonPrevNext[0].style.display = "none";
-      }
-      if (+item.innerText === 9) {
-        buttonPrevNext[1].style.display = "none";
-      } else {
-        buttonPrevNext[1].style.display = "flex";
-      }
-      buttonPrevNext[1].setAttribute("data-next-page", item.innerText);
-    });
-  });
-}
-
-function createContent(arrayOfArticles, numOfPage) {
+function createContent(arrayOfArticles, num) {
+  let numOfPage = 0;
+  if (num > 1) {
+    for (let i = 1; i < num; i++) {
+      numOfPage += 8;
+    }
+  }
   let arr = arrayOfArticles.slice(numOfPage, numOfPage + 8);
   if (numOfPage + 8 > arrayOfArticles.length) {
     arr = arrayOfArticles.slice(numOfPage, arrayOfArticles.length);
@@ -135,44 +129,13 @@ function createArticle(articleObj) {
   article.appendChild(imgLink);
   article.appendChild(text);
   blogContent.appendChild(article);
-}
-
-document.querySelectorAll(".blog-links").forEach(function(item) {
-  item.addEventListener("click", function(event) {
-    arrayOfArticles = [];
-    arrayOfArticles = arrayOfAllArticles.filter(function(item) {
-      return item.categorie === event.target.innerText;
-    });
-    document.querySelector(".blog-content").innerHTML = "";
-    createContent(arrayOfArticles, 0);
-    document.querySelector(".blog-pages").innerHTML = "";
-    createPages();
-    switchPages();
-    document.querySelector("h1").innerText = event.target.innerText;
-    document.querySelector(".nav-links-add").innerHTML =
-      "» " +
-      '<a href="blog.html#' +
-      event.target.innerText +
+  if (serchLine) {
+    document.querySelector("h1").innerText = articleObj.categorie;
+    document.querySelector(".nav-links-categorie").innerHTML =
+      ' » <a href="blog.html?' +
+      articleObj.categorie +
       '" class="nav-links">' +
-      " " +
-      event.target.innerText +
+      articleObj.categorie +
       "</a>";
-  });
-});
-
-function changePages(buttondata) {
-  const buttons = document.querySelectorAll("a.blog-pages-links");
-  let buttonIndex = +buttondata.getAttribute("data-next-page") - 1;
-  const direction = buttondata.getAttribute("direction");
-  if (buttonIndex === buttons.length - 1) {
-    document.querySelectorAll("p.blog-pages-links")[1].style.display = "none";
   }
-  if (direction === "prev") {
-    buttonIndex--;
-    buttondata.setAttribute("data-next-page", buttonIndex);
-  } else {
-    buttonIndex++;
-    buttondata.setAttribute("data-next-page", buttonIndex + 1);
-  }
-  buttons[buttonIndex].click();
 }
