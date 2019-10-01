04b11f62a987be3df20dbe4bde98d13f03514aa7
Roman Saldan <saldanroman@gmail.com>
2019-10-01 21:37:24 +0300

    articles.js restored

--- js/articles.js
+++ js/articles.js
@@ -1,60 +1,63 @@
-(function() {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.response).articles;
-      for (let i = 0; i < 3; i++) {
-        const randomArticleObj = ~~(Math.random() * (data.length - 1));
-        buildArticle(data[randomArticleObj]);
-      }
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    const articlesCounter = data["articles"].length;
+    const firstArticle = Math.floor(Math.random() * articlesCounter);
+    const secondArticle = Math.floor(Math.random() * articlesCounter);
+    const thirdArticle = Math.floor(Math.random() * articlesCounter);
+
+    const width = screen.width;
+    const articles = document.getElementsByClassName("articles")[0];
+    bildArticle(firstArticle);
+    bildArticle(secondArticle);
+    bildArticle(thirdArticle);
+    function bildArticle(articleIndex) {
+      let articlesContainer = document.createElement("div");
+      articlesContainer.classList.add("articles-container");
+
+      let imgLink = document.createElement("a");
+      imgLink.href = "articles.html?" + data["articles"][articleIndex].id;
+
+      let articlesImgContainer = document.createElement("div");
+      articlesImgContainer.classList.add("articles-img-container");
+
+      let img = document.createElement("img");
+      img.classList.add("articles-img");
+      img.src = data["articles"][articleIndex].image;
+      img.title = data["articles"][articleIndex].title;
+      articlesImgContainer.appendChild(img);
+      imgLink.appendChild(articlesImgContainer);
+      articlesContainer.appendChild(imgLink);
+
+      let articlesDate = document.createElement("span");
+      articlesDate.classList.add("articles-date");
+      articlesDate.innerHTML = data["articles"][articleIndex].date;
+      articlesContainer.appendChild(articlesDate);
+
+      let articlesDescription = document.createElement("div");
+      articlesDescription.classList.add("articles-description");
+
+      let articlesDescriptionTitle = document.createElement("span");
+      articlesDescriptionTitle.classList.add("articles-description-title");
+      articlesDescriptionTitle.innerHTML = data["articles"][articleIndex].title;
+      articlesDescription.appendChild(articlesDescriptionTitle);
+
+      let articlesDescriptionText = document.createElement("p");
+      articlesDescriptionText.classList.add("articles-description-text");
+      articlesDescriptionText.innerHTML = data["articles"][articleIndex].text;
+      articlesDescription.appendChild(articlesDescriptionText);
+
+      let articlesDescriptionId = document.createElement("a");
+      articlesDescriptionId.classList.add("articles-description-id");
+      articlesDescriptionId.innerHTML = "more details";
+      articlesDescriptionId.href =
+        "articles.html?" + data["articles"][articleIndex].id;
+      articlesDescription.appendChild(articlesDescriptionId);
+      articlesContainer.appendChild(articlesDescription);
+      articles.appendChild(articlesContainer);
     }
-  };
-  xhr.open("GET", "./jsons/articles.json", true);
-  xhr.send();
-})();
-
-function buildArticle(articleObject) {
-  const articles = document.querySelector(".articles");
-  let articlesContainer = document.createElement("div");
-  articlesContainer.classList.add("articles-container");
-
-  let imgLink = document.createElement("a");
-  imgLink.href = "article.html?" + articleObject.id;
-
-  let articlesImgContainer = document.createElement("div");
-  articlesImgContainer.classList.add("articles-img-container");
-
-  let img = document.createElement("img");
-  img.classList.add("articles-img");
-  img.src = articleObject.image;
-  img.title = articleObject.title;
-  articlesImgContainer.appendChild(img);
-  imgLink.appendChild(articlesImgContainer);
-  articlesContainer.appendChild(imgLink);
-
-  let articlesDate = document.createElement("span");
-  articlesDate.classList.add("articles-date");
-  articlesDate.innerHTML = articleObject.date;
-  articlesContainer.appendChild(articlesDate);
-
-  let articlesDescription = document.createElement("div");
-  articlesDescription.classList.add("articles-description");
-
-  let articlesDescriptionTitle = document.createElement("span");
-  articlesDescriptionTitle.classList.add("articles-description-title");
-  articlesDescriptionTitle.innerHTML = articleObject.title;
-  articlesDescription.appendChild(articlesDescriptionTitle);
-
-  let articlesDescriptionText = document.createElement("p");
-  articlesDescriptionText.classList.add("articles-description-text");
-  articlesDescriptionText.innerHTML = articleObject.text;
-  articlesDescription.appendChild(articlesDescriptionText);
-
-  let articlesDescriptionId = document.createElement("a");
-  articlesDescriptionId.classList.add("articles-description-id");
-  articlesDescriptionId.innerHTML = "more details";
-  articlesDescriptionId.href = "article.html?" + articleObject.id;
-  articlesDescription.appendChild(articlesDescriptionId);
-  articlesContainer.appendChild(articlesDescription);
-  articles.appendChild(articlesContainer);
-}
+  }
+};
+xmlhttp.open("GET", "jsons/articles.json", true);
+xmlhttp.send();
