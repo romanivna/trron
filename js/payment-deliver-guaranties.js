9414eb7e8b63b92c99d01785409f21cfe9d83180
OlecsandrStelmakh <saniast05@gmail.com>
2019-10-06 10:38:42 +0300

    fix feature/payment-deliver-guaraties

--- js/payment-deliver-guaranties.js
+++ js/payment-deliver-guaranties.js
@@ -3,19 +3,19 @@
   addPointTooBreadcrumbMap(breadcrumbs);
   const HEIGHT_MENU = 110;
 
-  const muveToSection = function() {
+  const moveToSection = function() {
     const sectionName = location.search.substr(1);
     const section = document.getElementById(sectionName);
-    const muveTo = section.offsetTop - HEIGHT_MENU;
-    for (let i = 0; i < muveTo; i++) {
-      function muve() {
-        myVar = setTimeout(function() {
+    const moveTo = section.offsetTop - HEIGHT_MENU;
+    for (let i = 0; i < moveTo; i++) {
+      function move() {
+        moveSpeed = setTimeout(function() {
           window.scrollTo(0, i);
           i++;
         }, i * 2);
       }
-      muve();
+      move();
     }
   };
-  muveToSection();
+  moveToSection();
 })();
