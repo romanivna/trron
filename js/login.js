3c66cf4a667581d8ff6dddad4923777975166d0b
Saldan Roman <saldanroman@gmail.com>
2019-10-08 01:07:39 +0300

    add img

--- js/login.js
+++ js/login.js
@@ -1,4 +1,20 @@
 let form = document.forms.login;
+let useresArrObj;
+(function () {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            useresArrObj = JSON.parse(xhr.response);
+            breadcrumbs = [{
+                name: "name",
+                link: "#"
+            }]
+            addPointTooBreadcrumbMap()
+        }
+    };
+    xhr.open("GET", "../jsons/users.json", true);
+    xhr.send();
+})();
 
 function logIn(event) {
     event.preventDefault();
